b1da8c863735defd1d8b1fa1c54e7c384b811ab1
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2022-10-06 19:05:20 -0400

    some custom changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import {CookiesProvider} from "react-cookie";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-function App() {
+import Typography from '@mui/material/Typography';
+
+import Header from './components/basic/Header'
+import Footer from './components/basic/Footer'
+
+const App = () => {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+    typography: {
+      fontFamily: [
+        'Helvetica Neue',
+        'HelveticaNeue-Medium',
+        'HelveticaNeue-Light',
+        'Roboto',
+        'sans-serif'
+      ].join(','),
+    },
+  });
+
   return (
-    <div className="App">
-    </div>
-  );
+    // <CookiesProvider>
+    <Router>
+      <React.StrictMode>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Header />
+          <Typography variant="h1">React Project</Typography>
+          <Footer />
+        </ThemeProvider>
+      </React.StrictMode>
+    </Router>
+    // </CookiesProvider> 
+  )
 }
 
-export default App;
+export default App
